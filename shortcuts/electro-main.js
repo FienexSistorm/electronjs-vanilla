931fa71611e1db52f69b198dbf753758b27cce88
FienexSistorm <70212249+FienexSistorm@users.noreply.github.com>
2022-04-10 22:39:45 +0000

    electron shortcuts

---
+++ shortcuts/electro-main.js
@@ -0,0 +1,57 @@
+// main electron application
+
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+
+
+let mainWindow;  // this is the main app window
+
+/**@method createMainWindow */
+const createMainWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 260,
+        center: true,
+        resizable: true,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            devTools: true
+        },
+        icon: `${__dirname}/assets/icon.ico`
+    });
+
+    mainWindow.loadFile("src/index.html");
+}
+
+
+/**@method configureShortcuts */
+const configureShortcuts = () => {
+    globalShortcut.register("CommanOrControl+R", () => {
+        console.log("Content refreshing...");
+
+    });
+    globalShortcut.register("Alt+CommandOrControl+I", () => {
+        console.log("DevTools shortcut is requested");
+    });
+    globalShortcut.register("CommandOrControl+F", () => {
+        console.log("Find in content");
+    })
+}
+
+app.whenReady().then(() => {
+    globalShortcut.register("CommandOrControl+R", () => {
+        console.log("Content refreshing...");
+
+    });
+    globalShortcut.register("Shift+CommandOrControl+I", () => {
+        console.log("DevTools shortcut is requested");
+    });
+    globalShortcut.register("CommandOrControl+F", () => {
+        console.log("Find in content");
+    })
+    console.log("creating the main window..");
+    createMainWindow();
+    configureShortcuts();
+    
+})
+
